c403bd31c74f28fa4f21b1000e00179b1234fa14
Andrew G <gountisa@roverkids.org>
2024-09-17 07:33:50 -0400

    Beautified Output Formatting

--- RaceWinners/RaceWinners/Program.cs
+++ RaceWinners/RaceWinners/Program.cs
@@ -22,7 +22,6 @@ public class Program
             Console.WriteLine($"{data[i].Name} - [{ranks}]");
         }
 
-        // find min number of ranks in all the groups
         int minRanks = data.Min(group => group.Ranks.Count);
 
         // Cut larger lists to make each group the same size
@@ -49,14 +48,25 @@ public class Program
             }
         }
 
-        // print out the averages
+        Console.WriteLine();
+
         for (int group = 0; group < data.Count; group++)
         {
-            Console.WriteLine($"Group {data[group].Name}'s average is: " + averages[group]);
+            string classAndAverage = $"Group {data[group].Name}'s average is: " + averages[group];
+            Console.SetCursorPosition((Console.WindowWidth - classAndAverage.Length) / 2, Console.CursorTop);
+            Console.WriteLine(classAndAverage);
         }
 
         Console.WriteLine();
 
-        Console.WriteLine($"Group {data[lowestAverageIndex].Name} Wins!");
+        Console.ForegroundColor = ConsoleColor.Green;
+
+        string winningClass = $"{data[lowestAverageIndex].Name} Wins!";
+
+        Console.SetCursorPosition((Console.WindowWidth - winningClass.Length) / 2, Console.CursorTop);
+
+        Console.WriteLine(winningClass);
+
+        Console.ResetColor();
     }
 }
