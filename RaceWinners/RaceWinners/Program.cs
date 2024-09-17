48f96ff5e4747d2e197301e70e0a72a1f53f608f
Andrew G <gountisa@roverkids.org>
2024-09-17 07:35:27 -0400

    Added Comments

--- RaceWinners/RaceWinners/Program.cs
+++ RaceWinners/RaceWinners/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 
 namespace RaceWinners;
@@ -21,7 +22,8 @@ public class Program
             
             Console.WriteLine($"{data[i].Name} - [{ranks}]");
         }
-
+        
+        // find the length of the smallest group
         int minRanks = data.Min(group => group.Ranks.Count);
 
         // Cut larger lists to make each group the same size
@@ -50,9 +52,11 @@ public class Program
 
         Console.WriteLine();
 
+        // print out the average of each class
         for (int group = 0; group < data.Count; group++)
         {
             string classAndAverage = $"Group {data[group].Name}'s average is: " + averages[group];
+            // center the text in the console
             Console.SetCursorPosition((Console.WindowWidth - classAndAverage.Length) / 2, Console.CursorTop);
             Console.WriteLine(classAndAverage);
         }
@@ -63,6 +67,7 @@ public class Program
 
         string winningClass = $"{data[lowestAverageIndex].Name} Wins!";
 
+        // center the text in the console
         Console.SetCursorPosition((Console.WindowWidth - winningClass.Length) / 2, Console.CursorTop);
 
         Console.WriteLine(winningClass);
